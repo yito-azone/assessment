4a3e7826643c5aeea7252531c74cf6ffc2195b25
yito-azone <132737498+yito-azone@users.noreply.github.com>
2023-10-07 11:28:08 +0900

    BootStrap使用

--- assessment.js
+++ assessment.js
@@ -15,15 +15,33 @@ function onclick() {
 
   resultDivision.innerText = "";
 
-  const header = document.createElement("h3");
-  header.innerText = "診断結果";
-  resultDivision.appendChild(header);
+  // const header = document.createElement("h3");
+  // header.innerText = "診断結果";
+  // resultDivision.appendChild(header);
+
+  // const paragraph = document.createElement("p");
+  // const result = assessment(userName);
+
+  // paragraph.innerText = result;
+  // resultDivision.appendChild(paragraph);
+
+  const headerDivision = document.createElement("div");
+  headerDivision.setAttribute("class", "card-header text-bg-primary");
+  headerDivision.innerText = "診断結果";
+
+  const bodyDivision = document.createElement("div");
+  bodyDivision.setAttribute("class", "card-body");
 
   const paragraph = document.createElement("p");
+  paragraph.setAttribute("class", "card-text");
   const result = assessment(userName);
-
   paragraph.innerText = result;
-  resultDivision.appendChild(paragraph);
+  bodyDivision.appendChild(paragraph);
+
+  resultDivision.setAttribute("class", "card");
+
+  resultDivision.appendChild(headerDivision);
+  resultDivision.appendChild(bodyDivision);
 
   tweetDivision.innerText = "";
   const anchor = document.createElement("a");
